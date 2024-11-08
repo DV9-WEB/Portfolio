c668297d430069480aafb75c133dbfe9272ebb8e
Deepanshu Verma <157585273+DV9-WEB@users.noreply.github.com>
2024-11-08 13:27:27 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -176,7 +176,7 @@ const Navbar = () => {
             <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
           </span>
           <div className="relative flex space-x-1 items-center z-10 rounded-full bg-transparent py-2 px-4 ring-1 ring-white/10 ">
-            <span>Read Resume</span>
+            <a href="https://drive.google.com/file/d/1TGv-fG_C0wi2b_wuzyKh-hncgSWhHEXw/view?usp=sharing">Read Resume</a>
             <svg
               width="16"
               height="16"
